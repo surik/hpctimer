d37f7f95e10f0d86fce61ef6df77323be110624d
surik <surikx@gmail.com>
2011-04-06 01:18:13 +0700

    added HPCTIMER_TSC without frequency

--- hpctimer.c
+++ hpctimer.c
@@ -36,7 +36,7 @@ void hpctimer_free(hpctimer_t *timer)
 
 double hpctimer_wtime(hpctimer_t *timer) 
 {
-    return timer->gettime() * 0.000001;
+    return timer->gettime() * 0.000001; /* convert to seconds */
 }
 
 static unsigned long long hpctimer_gettimeofday()
@@ -52,6 +52,15 @@ static unsigned long long hpctimer_gettimeofday()
 
 static unsigned long long hpctimer_gettsc()
 {
-    /* RDTSC */
-    return 0;
+    unsigned long long low, high;
+
+    __asm__ volatile(
+        "rdtsc\n"
+        "mov %%eax, %0\n"
+        "mov %%edx, %1\n" 
+        : "=m" (low), "=m" (high)
+    );
+    
+    /* returns the number of ticks, needed to divide on frequerence cpu */
+    return (unsigned long long) high << 32 | low;
 }
